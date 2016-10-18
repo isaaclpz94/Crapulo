40fb965c303176038519b9075ad3730f34ddbe7e
isaaclpz94 <isaaclopez2894@hotmail.com>
2016-10-18 19:13:33 +0200

    Crapulo - OC
    Calcular el crapulo de un numero

--- Crapulo/AppDelegate.swift
+++ Crapulo/AppDelegate.swift
@@ -9,10 +9,12 @@
 import UIKit
 import CoreData
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
--- Crapulo/ViewController.swift
+++ Crapulo/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var suma : Int = 0
+    var primero = true
+    var numero:String = ""
 
+    @IBOutlet var TFNumero: UITextField!
+    
+    @IBOutlet var LBRes: UILabel!
+    
+    @IBAction func BTNRes(sender: UIButton) {
+        if TFNumero.hasText(){ //si el user ha escrito algo en el texfield
+            if isValidEmail(TFNumero.text){
+                
+            
+            
+            primero = true
+            
+            do{
+                
+                if primero{ //si es la primera vez
+                    numero = TFNumero.text
+                    primero=false
+                }else{
+                    numero = String(suma)
+                }
+                var array  = [Character]()
+            
+                for index in numero{
+                    array.append(index)
+                }
+            
+                println("Los numeros son: \(array)")
+            
+                suma = 0
+                for num in array{
+                    var numero:Int = String(num).toInt()! //Caracter to int
+                    suma = suma + numero
+                }
+            }while suma > 10
+                
+            LBRes.text = String(suma)
+            }else{
+                LBRes.text = "Inserte un número válido"
+            }
+            
+        }else{
+            LBRes.text = "Inserte un número"
+        }
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+
+        let emailRegEx = "[0-9]+"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(testStr)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
